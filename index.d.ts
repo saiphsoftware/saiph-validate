12a563ac25e52ca277ca64743585c23b30c255d8
Dylan Bartley <d.bartley44@gmail.com>
2024-05-12 06:27:51 -0400

    fixed typing

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,6 @@
 import { ValidateFn } from "@saiphsoft/validate/validators";
 
-export default function validate ( value: any, validators: ValidatorFn[] ): ValidationResult;
-
-type ValidatorFn = ( ...args: any[] ) => ValidateFn;
+export default function validate ( value: any, validators: ValidateFn[] ): ValidationResult;
 
 interface ValidationResult {
   valid: boolean;
